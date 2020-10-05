32a66dc3694d8db55d3d2192d1762360dbdbd0e8
Renat Valeev <valik.valeevrenat@mail.ru>
2020-10-05 14:47:51 +0300

    added some lines about socket to client

--- client/client.py
+++ client/client.py
@@ -1,7 +1,13 @@
-'''import time
-import request
-import os
-'''
+import socket, os, sys
+
+
+SEPARATOR = "<SEPARATOR>" # separator for filename and size transferring
+BUFFER_SIZE = 4096 # send 4096 bytes each time step
+
+# s = socket.socket()
+# s.connect((host, port))
+# s.send(f"{filename}{SEPARATOR}{filesize}".encode())
+# s.sendall(bytes_read)
 
 class Client():
 
